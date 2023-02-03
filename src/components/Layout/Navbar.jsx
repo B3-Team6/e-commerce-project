66452f47f1b160b7e8a1e2356c9d52b58babcfa9
LouisLagrange1 <ljblagrange@gmail.com>
2023-02-03 19:58:42 +0100

    catégories + layout

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,19 +1,19 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { ShoppingCartIcon } from "@heroicons/react/24/solid";
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid"
+import { ShoppingCartIcon } from "@heroicons/react/24/solid"
+import { Bars3Icon } from "@heroicons/react/24/solid"
 
 const Navbar = () => {
-  
   return (
     <>
       <div className="bg-orange-100 flex justify-between p-2 sticky top-0">
-      
-        <div className="font-serif font-bold text-2xl pl-2">AIRNES</div>
+        <a href="#" className="font-serif font-bold text-2xl pl-2">
+          AIRNES
+        </a>
         <div className="flex gap-4 ">
-          <MagnifyingGlassIcon className="h-8 w-8" />
-          <ShoppingCartIcon className="h-8 w-8" />
-          <Bars3Icon className="h-8 w-8" />
-        </div> 
+          <MagnifyingGlassIcon className="h-8 w-8 hover:text-white" />
+          <ShoppingCartIcon className="h-8 w-8  hover:text-white" />
+          <Bars3Icon className="h-8 w-8  hover:text-white" />
+        </div>
       </div>
     </>
   )
