2ee1b602df605373db8821cabf06ef925cd85ce6
LouisLagrange1 <ljblagrange@gmail.com>
2023-02-09 14:53:29 +0100

    description done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,27 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
 
+const descriptions = [
+  {
+    id: 1,
+    name: "Canape 1",
+    price: "XXX €",
+    statut: "EN STOCK",
+    description:
+      "cbyazyacubacabcuabcuabcuabcaucbucbaucbaucbaucbucaczcjziacjzaicjzai",
+    img: (
+      <Image
+        alt="canape"
+        width={400}
+        height={400}
+        src="/images/canape.jpg"
+        className="rounded-lg hover:opacity-50"
+      />
+    ),
+  },
+];
 
-const Home = ()=> {
+const Home = () => {
   return (
     <>
       <Head>
@@ -10,8 +30,30 @@ const Home = ()=> {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    
+
+      <div className="flex justify-end p-4">
+        {descriptions.map((description) => (
+          <>
+            <div className="flex flex-col">
+              <div className="flex justify-between ">
+                <div className=" flex  flex-col ">{description.price}</div>
+                <div className="">
+                  {description.name}
+                  <div className="text-lime-400 pb-2">{description.statut}</div>
+                </div>
+              </div>
+
+              {description.description}
+              <div className="flex justify-center ">
+                <button className="bg-orange-100 px-2 w-fit border-2 border-black rounded-lg font-bold m-8 ">
+                  Ajouter au panier
+                </button>
+              </div>
+            </div>
+          </>
+        ))}
+      </div>
     </>
-  )
-}
-export default  Home
+  );
+};
+export default Home;
