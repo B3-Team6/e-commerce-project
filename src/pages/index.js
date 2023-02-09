a79a20d5bae85ed8ce8d31868beba31bba72d4f2
LouisLagrange1 <ljblagrange@gmail.com>
2023-02-09 11:21:43 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import Head from "next/head";
+import Image from "next/image";
 
 const products = [
   {
     id: 1,
-    name: "Name 1",
+    name: "Canapé 1",
     price: "111 €",
     img: (
       <Image
@@ -19,7 +18,7 @@ const products = [
   },
   {
     id: 2,
-    name: "Name 2",
+    name: "Canapé 2",
     price: "222 €",
     img: (
       <Image
@@ -33,7 +32,7 @@ const products = [
   },
   {
     id: 3,
-    name: "Name 3",
+    name: "Canapé 3",
     price: "333 €",
     img: (
       <Image
@@ -47,7 +46,7 @@ const products = [
   },
   {
     id: 4,
-    name: "Name 4",
+    name: "Canapé 4",
     price: "444 €",
     img: (
       <Image
@@ -61,7 +60,7 @@ const products = [
   },
   {
     id: 5,
-    name: "Name 5",
+    name: "Canapé 5",
     price: "555 €",
     img: (
       <Image
@@ -75,7 +74,7 @@ const products = [
   },
   {
     id: 6,
-    name: "Name 6",
+    name: "Canapé 6",
     price: "666 €",
     img: (
       <Image
@@ -89,7 +88,7 @@ const products = [
   },
   {
     id: 7,
-    name: "Name 7",
+    name: "Canapé 7",
     price: "777 €",
     img: (
       <Image
@@ -103,7 +102,7 @@ const products = [
   },
   {
     id: 8,
-    name: "Name 8",
+    name: "Canapé 8",
     price: "888 €",
     img: (
       <Image
@@ -115,12 +114,9 @@ const products = [
       />
     ),
   },
-
 ];
 
 const Home = () => {
-
-
   return (
     <>
       <Head>
@@ -130,8 +126,8 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className='flex  justify-center '>
-        <div className='flex group items-center justify-center '>
+      <div className="flex  justify-center ">
+        <div className="flex group items-center justify-center ">
           <Image
             alt="canape"
             width={400}
@@ -139,45 +135,33 @@ const Home = () => {
             src="/images/canape.jpg"
             className="rounded-3xl hover:opacity-50  w-full p-4 "
           />
-          <div className='hidden group-hover:flex absolute text-3xl font-black'>Canapé</div>
+          <div className="hidden group-hover:flex absolute text-3xl font-black">
+            Canapé
+          </div>
         </div>
-
       </div>
-      <div className='p-2 flex justify-center font-bold text-2xl font-serif bg-slate-200'>
-        <p>Voici notre collection de canapés, cliquez sur un produit pour voir sa description</p>
+      <div className="p-2 flex justify-center font-bold text-2xl font-serif bg-slate-200">
+        <p>
+          Voici notre collection de canapés, cliquez sur un produit pour voir sa
+          description
+        </p>
       </div>
 
-
-
-
-      <div className="grid grid-cols-4 gris-rows-3 gap-3 p-4 items-center ">
+      <div className="grid grid-cols-4 gris-rows-3 gap-3 p-4 items-center">
         {products.map((product) => (
           <>
-            <div className="flex items-center flex-col">
-              <div className="flex  items-center w-4/5">
-                {product.img}
-              </div>
-              <div className='flex items-center justify-between  w-72'>
-                <a >
-                  {product.name}
-                </a>
+            <div className="flex items-center flex-col ">
+              <div className="flex  items-center w-4/5">{product.img}</div>
+              <div className="flex items-center justify-between w-72">
+                <a>{product.name}</a>
                 &nbsp;
-                <a >
-                  {product.price}
-                </a>
-
+                <a>{product.price}</a>
               </div>
             </div>
-
           </>
         ))}
       </div>
-
-
-
-
-
     </>
-  )
-}
-export default Home
+  );
+};
+export default Home;
