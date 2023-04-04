686b7e8e66ad4b559e01cce1aab3569cc85d58d5
MarvinSABIN <sabin.marvin972@gmail.com>
2023-04-04 19:48:56 +0200

    changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,35 +1,35 @@
-import Image from "next/image";
-import React, { useState, useEffect } from "react";
+import Image from "next/image"
+import React, { useState, useEffect } from "react"
 
 const Carousel = () => {
   const images = [
     { id: 1, src: "images/lit.jpg" },
     { id: 2, src: "images/canape.jpg" },
     { id: 3, src: "images/table.jpg" },
-  ];
-  const [currentIndex, setCurrentIndex] = useState(0);
+  ]
+  const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setCurrentIndex((currentIndex) => (currentIndex + 1) % images.length);
-    }, 2300);
-    return () => clearInterval(intervalId);
-  }, [images.length]);
+      setCurrentIndex((currentIndex) => (currentIndex + 1) % images.length)
+    }, 2300)
+    return () => clearInterval(intervalId)
+  }, [images.length])
 
   return (
     <div className="flex justify-center">
       <div className=" w-1/2 overflow-hidden p-4">
         {images.map((item, index) => {
-          const isActive = index === currentIndex;
+          const isActive = index === currentIndex
           return (
             <div key={item.id} style={{ display: isActive ? "block" : "none" }}>
               <img className="rounded-3xl" src={item.src} alt={item.id} />
             </div>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Carousel;
+export default Carousel
