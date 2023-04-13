d4408e026c2c1f12af18277eeb0edf3e3c355b61
MarvinSABIN <sabin.marvin972@gmail.com>
2023-04-13 23:26:00 +0200

    minor changes

--- src/components/Layout/CartPage.jsx
+++ src/components/Layout/CartPage.jsx
@@ -1,119 +1,168 @@
-import { useState } from "react"
+import React, { useState } from "react"
+import Wrapper from "./Wrapper"
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([
     {
-      name: "Chaise",
-      quantity: 10,
-      price: 799,
-      image: "https://dummyimage.com/100x100/000/fff",
+      id: 1,
+      name: "Lit",
+      price: 199.99,
+      image:
+        "https://www.cdiscount.com/pdt2/6/8/8/1/700x700/vid3181049686688/rw/meuble-cadre-de-lit-double-structure-de-lit-adul.jpg",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      quantity: 2,
     },
     {
-      name: "Chaise",
+      id: 2,
+      name: "Chaise blanc",
+      price: 29.99,
+      image:
+        "https://www.ikea.com/fr/fr/images/products/fanbyn-chaise-blanc-interieur-exterieur__1052803_pe846425_s5.jpg?f=s",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac turpis eget enim aliquam commodo.",
       quantity: 1,
-      price: 99,
-      image: "https://dummyimage.com/100x100/000/fff",
+      shipping: 9.99,
+      tax: 1.99,
     },
     {
-      name: "Souris",
-      quantity: 3,
-      price: 49,
-      image: "https://dummyimage.com/100x100/000/fff",
+      id: 3,
+      name: "Table Basse",
+      price: 99.99,
+      image:
+        "https://pierimport.imgix.net/produits/table-basse-moderne-bois-pied-en-croix-volga-62cd87aa19b4f.jpg?",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      quantity: 1,
     },
   ])
 
-  const removeItem = (index) => {
-    setCartItems((prevItems) => {
-      const newItems = [...prevItems]
-      newItems.splice(index, 1)
-
-      return newItems
-    })
+  const removeItem = (itemId) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item.id !== itemId))
   }
 
-  const updateQuantity = (index, quantity) => {
-    setCartItems((prevItems) => {
-      const newItems = [...prevItems]
-      newItems[index].quantity = quantity
-
-      return newItems
-    })
+  const updateQuantity = (itemId, newQuantity) => {
+    setCartItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: newQuantity } : item
+      )
+    )
   }
 
-  const totalPrice = cartItems.reduce((total, item) => {
-    return total + item.quantity * item.price
-  }, 0)
+  const calculateTax = (subTotal) => {
+    const taxRate = 0.2 // 20% tax
+    const tax = subTotal * taxRate
+
+    return tax
+  }
+
+  const calculateShipping = (subTotal) => {
+    const shippingRate = 0.1 // 10% shipping fee
+    const shipping = subTotal * shippingRate
+
+    return shipping
+  }
+
+  const subTotal = cartItems.reduce(
+    (accumulator, currentItem) =>
+      accumulator + currentItem.price * currentItem.quantity,
+    0
+  )
+
+  const tax = calculateTax(subTotal)
+  const shipping = calculateShipping(subTotal)
+  const total = subTotal + tax + shipping
 
   return (
-    <div className="text-black bg-white">
-      <h1 className="text-5xl font-semibold text-center p-6 sm:p-12">Panier</h1>
-      <div className="lg:flex lg:flex-row lg:flex-wrap lg:items-center lg:justify-between lg:mx-40 lg:mt-8">
-        <div className="flex-col w-full max-w-md mb-10 sm:mb-20">
-          {cartItems.map((item, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-between border-b border-gray-400 py-4"
-            >
-              <div className="flex items-center">
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className="w-16 h-16 object-cover mx-3 rounded-2xl"
-                />
-                <div className="flex flex-col">
-                  <p className="font-semibold py-1 mb-2 px-4">{item.name}</p>
-                  <div className="flex items-center">
-                    <button
-                      className="px-3 py-1 rounded-full bg-gray-200 text-gray-800 hover:bg-gray-300 mr-2 sm:mr-4"
-                      onClick={() => updateQuantity(index, item.quantity - 1)}
-                      disabled={item.quantity === 1}
+    <>
+      <Wrapper>
+        <div className="flex items-center justify-center h-screen">
+          {cartItems.length === 0 ? (
+            <p className="text-center text-4xl font-bold mt-10">
+              Votre panier est vide
+              <img
+                src="https://media.istockphoto.com/id/861576608/vector/empty-shopping-bag-icon-online-business-vector-icon-template.jpg?s=612x612&w=0&k=20&c=I7MbHHcjhRH4Dy0NVpf4ZN4gn8FVDnwn99YdRW2x5k0="
+                alt=""
+              />
+            </p>
+          ) : (
+            <div className="flex flex-row">
+              <div className="w-3/4 p-4">
+                <div className="border-b mb-4 pb-2 mr-40">
+                  <h1 className="text-2xl font-bold">Votre Panier</h1>
+                </div>
+                <div className="flex flex-col space-y-4 mr-40">
+                  {/* List of Cart Items */}
+                  {cartItems.map((item) => (
+                    <div
+                      key={item.id}
+                      className="flex items-center justify-between border-b pb-2"
                     >
-                      -
-                    </button>
-                    <span className="text-lg">{item.quantity}</span>
-                    <button
-                      className="px-3 py-1 rounded-full bg-gray-200 text-gray-800 hover:bg-gray-300 ml-2 sm:ml-4"
-                      onClick={() => updateQuantity(index, item.quantity + 1)}
-                    >
-                      +
-                    </button>
-                  </div>
+                      <div className="flex items-center space-x-2">
+                        <img
+                          className="w-16 h-16 object-cover rounded-md"
+                          src={item.image}
+                          alt={item.name}
+                        />
+                        <div>
+                          <h2 className="font-medium text-lg">{item.name}</h2>
+                          <p className="text-sm text-gray-500">
+                            {item.description}
+                          </p>
+                          <p className="font-medium">{item.price}€</p>
+                        </div>
+                      </div>
+                      <div className="flex items-center space-x-2">
+                        <button
+                          className="px-2 py-1 bg-black text-white rounded-md"
+                          onClick={() => removeItem(item.id)}
+                        >
+                          Supprimer
+                        </button>
+                        <input
+                          type="number"
+                          min="1"
+                          max="10"
+                          value={item.quantity}
+                          onChange={(e) =>
+                            updateQuantity(item.id, parseInt(e.target.value))
+                          }
+                          className="w-12 px-2 py-1 text-center border rounded-md"
+                        />
+                      </div>
+                    </div>
+                  ))}
                 </div>
               </div>
-              <button
-                className="px-3 py-1 rounded-full bg-gray-500 text-white hover:bg-gray-600 ml-4 hidden sm:block"
-                onClick={() => removeItem(index)}
-              >
-                Retirer l'article
-              </button>
+              <div className="w-1/4 p-4 bg-gray-100 rounded-md">
+                <div className="border-b mb-4 pb-2">
+                  <h2 className="text-xl font-medium">Total</h2>
+                </div>
+                <div>
+                  <p className="flex justify-between">
+                    <span className="font-medium">Sous-total:</span>
+                    <span>{subTotal.toFixed(2)}€</span>
+                  </p>
+                  <p className="flex justify-between">
+                    <span className="font-medium">Livraison:</span>
+                    <span>{shipping.toFixed(2)}€</span>
+                  </p>
+                  <p className="flex justify-between">
+                    <span className="font-medium">Taxe:</span>
+                    <span>{tax.toFixed(2)}€</span>
+                  </p>
+                  <p className="flex justify-between mt-2">
+                    <span className="font-bold">Total:</span>
+                    <span className="font-bold">{total.toFixed(2)}€</span>
+                  </p>
+                  <button className="w-full mt-4 px-4 py-2 bg-black text-white rounded-md">
+                    Commander
+                  </button>
+                </div>
+              </div>
             </div>
-          ))}
+          )}
         </div>
-        {cartItems.length === 0 && (
-          <p className="text-gray-500 text-5xl font-semibold text-center p-12">
-            Le panier est vide
-          </p>
-        )}
-        {cartItems.length > 0 && (
-          <div className="flex flex-col items-center w-full max-w-md mt-8">
-            <div className="flex items-center justify-between w-full mb-6">
-              <p className="font-bold text-lg sm:text-2xl">Total</p>
-              <p className="font-bold text-lg sm:text-3xl">{totalPrice} €</p>
-            </div>
-            <button
-              className={`w-full px-6 py-2 rounded-full text-white font-semibold ${
-                cartItems.length === 0
-                  ? "bg-gray-400 cursor-not-allowed"
-                  : "bg-amber-400 hover:bg-amber-500"
-              }`}
-              disabled={cartItems.length === 0}
-            >
-              Passer la commande
-            </button>
-          </div>
-        )}
-      </div>
-    </div>
+      </Wrapper>
+    </>
   )
 }
 
