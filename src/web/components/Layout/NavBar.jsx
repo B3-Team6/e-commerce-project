ac55b17737cefd1910a39f68316aaf27c342eb1d
MarvinSABIN <sabin.marvin972@gmail.com>
2023-05-17 14:37:10 +0200

    Major Changes : - AddToCart, -removeFromCart -updateQuantity -Little Responsive for the page Cart

--- src/web/components/Layout/NavBar.jsx
+++ src/web/components/Layout/NavBar.jsx
@@ -1,6 +1,6 @@
 import useAppContext from "@/web/hooks/useAppContext"
 import routes from "@/web/routes.js"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import {
   MagnifyingGlassIcon,
   XMarkIcon,
@@ -8,6 +8,7 @@ import {
   Bars3Icon,
 } from "@heroicons/react/24/outline"
 import Link from "next/link"
+import { CartContext } from "@/web/hooks/CartContext"
 
 const NavBar = () => {
   const [isOpen, setOpen] = useState(false)
@@ -34,6 +35,23 @@ const NavBar = () => {
     setOpen(false)
   }
 
+  const { cart, setCart } = useContext(CartContext)
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart")
+
+    if (savedCart) {
+      const parsedCart = JSON.parse(savedCart)
+
+      setCart(parsedCart)
+    }
+  }, [setCart])
+
+  const cartCount = Object.values(cart).reduce(
+    (total, item) => total + item.quantity,
+    0
+  )
+
   return (
     <>
       <div className="flex items-center justify-between border-b border-gray-400 bg-[#F6E6D1] p-3 font-[Mulish] font-semibold">
@@ -52,9 +70,14 @@ const NavBar = () => {
               <Link href={routes.cart()}>
                 <button className="group relative flex items-center justify-center rounded-md p-2 text-black hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-400">
                   <ShoppingCartIcon
-                    className="h-8 w-10 lg:mr-6"
+                    className="h-10 w-10 lg:mr-6"
                     aria-hidden="true"
                   />
+                  {cartCount > 0 && (
+                    <span className="absolute -top-2 flex h-6 w-6 items-center justify-center rounded-full bg-black text-xs text-white">
+                      {cartCount}
+                    </span>
+                  )}
                 </button>
               </Link>
             </div>
