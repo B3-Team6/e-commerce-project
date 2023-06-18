1690595c6a0f5939128c10eb35b27be96f372d8f
LouisLagrange1 <ljblagrange@gmail.com>
2023-06-19 01:23:58 +0200

    edit seed

--- src/api/db/seeds/contact.js
+++ src/api/db/seeds/contact.js
@@ -2,259 +2,44 @@ exports.seed = async function (knex) {
   await knex.raw("TRUNCATE TABLE contact RESTART IDENTITY CASCADE")
   await knex("contact").insert([
     {
-      email: "info@example.com",
-      message: "Bonjour, j'ai une question sur votre produit.",
+      email: "marie@example.com",
+      message:
+        "Bonjour, j'ai récemment visité votre site de vente de meubles et j'ai quelques questions sur vos canapés. Pouvez-vous me donner plus de détails sur les options de personnalisation et les délais de livraison ? Merci !",
     },
     {
-      email: "samantha@example.com",
-      message: "Salut Samantha, comment ça va ?",
+      email: "alexandre@example.com",
+      message:
+        "Salut, je suis intéressé par votre collection de tables basses en bois massif. Pourriez-vous me dire si elles sont fabriquées à partir de bois durable et quelles sont les dimensions disponibles ? Je cherche quelque chose qui s'accorde bien avec mon salon. Merci d'avance !",
     },
     {
-      email: "robert@example.com",
-      message: "Bonjour Robert, j'ai besoin de votre aide.",
+      email: "laurence@example.com",
+      message:
+        "Bonjour, je suis en train de me meubler et je recherche une armoire spacieuse avec des étagères réglables. Est-ce que vous avez des modèles disponibles dans des finitions en chêne ou en noyer ? Je voudrais également savoir si vous proposez un service d'assemblage. Merci beaucoup !",
+    },
+    {
+      email: "vincent@example.com",
+      message:
+        "Salut, j'aimerais acheter une nouvelle table à manger pour ma cuisine. Est-ce que vous avez des tables extensibles qui peuvent accueillir jusqu'à 8 personnes ? Je suis également curieux de connaître les matériaux utilisés pour les plateaux de table et les options de couleur disponibles. Merci et bonne journée !",
     },
     {
       email: "sophie@example.com",
-      message: "Bonjour Sophie, je voulais vous remercier pour votre soutien.",
-    },
-    {
-      email: "maxime@example.com",
-      message: "Salut Maxime, j'espère que tu passes une bonne journée !",
-    },
-    {
-      email: "alice@example.com",
-      message: "Bonjour Alice, avez-vous reçu mon dernier e-mail ?",
-    },
-    {
-      email: "pierre@example.com",
-      message: "Bonjour Pierre, j'ai une proposition commerciale à vous faire.",
-    },
-    {
-      email: "isabelle@example.com",
-      message: "Salut Isabelle, on se voit ce soir ?",
-    },
-    {
-      email: "luc@example.com",
-      message: "Bonjour Luc, j'aimerais discuter d'un projet avec vous.",
-    },
-    {
-      email: "audrey@example.com",
-      message: "Salut Audrey, comment se passe votre nouvelle entreprise ?",
-    },
-    {
-      email: "julien@example.com",
       message:
-        "Bonjour Julien, pourriez-vous m'envoyer les détails de la réunion ?",
-    },
-    {
-      email: "laura@example.com",
-      message:
-        "Salut Laura, je voulais juste vous dire que vous avez fait un excellent travail.",
-    },
-    {
-      email: "mathieu@example.com",
-      message:
-        "Bonjour Mathieu, j'ai quelques questions concernant votre service.",
-    },
-    {
-      email: "camille@example.com",
-      message:
-        "Bonjour Camille, avez-vous des disponibilités pour une réunion la semaine prochaine ?",
-    },
-    {
-      email: "antoine@example.com",
-      message:
-        "Salut Antoine, je voulais vous féliciter pour votre promotion !",
-    },
-    {
-      email: "claire@example.com",
-      message: "Bonjour Claire, j'ai besoin de votre avis sur un projet.",
-    },
-    {
-      email: "arthur@example.com",
-      message:
-        "Bonjour Arthur, quand est-ce que vous êtes disponible pour une discussion téléphonique ?",
-    },
-    {
-      email: "léa@example.com",
-      message: "Salut Léa, j'aimerais vous inviter à notre événement spécial.",
-    },
-    {
-      email: "victor@example.com",
-      message:
-        "Bonjour Victor, pourriez-vous me recommander un bon restaurant dans la région ?",
-    },
-    {
-      email: "emma@example.com",
-      message: "Bonjour Emma, félicitations pour votre diplôme !",
-    },
-    {
-      email: "alexander@example.com",
-      message: "Hello Alexander, how are you doing?",
-    },
-    {
-      email: "natalie@example.com",
-      message: "Hi Natalie, I have a question about your recent presentation.",
-    },
-    {
-      email: "benjamin@example.com",
-      message: "Hello Benjamin, thank you for your assistance.",
-    },
-    {
-      email: "victoria@example.com",
-      message: "Hi Victoria, I wanted to congratulate you on your achievement.",
-    },
-    {
-      email: "nathan@example.com",
-      message: "Hello Nathan, I hope you're having a great day!",
-    },
-    {
-      email: "emma@example.com",
-      message: "Hi Emma, did you receive my email?",
+        "Bonjour, je suis à la recherche d'un canapé d'angle confortable pour mon salon. Pouvez-vous me recommander des modèles qui offrent un bon soutien lombaire et qui sont disponibles dans des tissus résistants aux taches ? J'apprécierais également des informations sur les garanties et les politiques de retour. Merci beaucoup !",
     },
     {
       email: "thomas@example.com",
-      message: "Hello Thomas, I have a business proposal for you.",
-    },
-    {
-      email: "sophia@example.com",
-      message: "Hi Sophia, are we still meeting tonight?",
-    },
-    {
-      email: "gabriel@example.com",
-      message: "Hello Gabriel, I would like to discuss a project with you.",
-    },
-    {
-      email: "olivia@example.com",
-      message: "Hi Olivia, how is your new startup going?",
-    },
-    {
-      email: "noah@example.com",
-      message: "Hello Noah, could you send me the details of the meeting?",
-    },
-    {
-      email: "emily@example.com",
       message:
-        "Hi Emily, I just wanted to say that you've done an excellent job.",
+        "Salut, je suis en train de rénover ma chambre à coucher et je suis à la recherche d'une commode avec beaucoup d'espace de rangement. Pouvez-vous me donner des détails sur les dimensions, les options de couleur et les matériaux utilisés pour vos commodes ? J'apprécierais également des conseils sur l'entretien. Merci d'avance !",
     },
     {
-      email: "samuel@example.com",
-      message: "Hello Samuel, I have some questions regarding your service.",
-    },
-    {
-      email: "charlotte@example.com",
+      email: "céline@example.com",
       message:
-        "Hi Charlotte, do you have any availability for a meeting next week?",
+        "Bonjour, je suis intéressée par votre collection de chaises de salle à manger. Pouvez-vous me dire si elles sont vendues par lot et si vous proposez des options de rembourrage personnalisé ? Je cherche des chaises qui offrent un bon soutien du dos et qui sont faciles à nettoyer. Merci et bonne journée !",
     },
     {
-      email: "gabrielle@example.com",
-      message: "Hello Gabrielle, congratulations on your promotion!",
-    },
-    {
-      email: "leo@example.com",
-      message: "Hi Leo, I need your opinion on a project.",
-    },
-    {
-      email: "julia@example.com",
-      message: "Hello Julia, when are you available for a phone call?",
-    },
-    {
-      email: "lucas@example.com",
-      message: "Hi Lucas, I would like to invite you to our special event.",
-    },
-    {
-      email: "clara@example.com",
-      message: "Hello Clara, can you recommend a good restaurant in the area?",
-    },
-    {
-      email: "adam@example.com",
-      message: "Hi Adam, congratulations on your graduation!",
-    },
-    {
-      email: "alexander@example.com",
-      message: "Hello Alexander, how are you doing?",
-    },
-    {
-      email: "natalie@example.com",
-      message: "Hi Natalie, I have a question about your recent presentation.",
-    },
-    {
-      email: "benjamin@example.com",
-      message: "Hello Benjamin, thank you for your assistance.",
-    },
-    {
-      email: "victoria@example.com",
-      message: "Hi Victoria, I wanted to congratulate you on your achievement.",
-    },
-    {
-      email: "nathan@example.com",
-      message: "Hello Nathan, I hope you're having a great day!",
-    },
-    {
-      email: "emma@example.com",
-      message: "Hi Emma, did you receive my email?",
-    },
-    {
-      email: "thomas@example.com",
-      message: "Hello Thomas, I have a business proposal for you.",
-    },
-    {
-      email: "sophia@example.com",
-      message: "Hi Sophia, are we still meeting tonight?",
-    },
-    {
-      email: "gabriel@example.com",
-      message: "Hello Gabriel, I would like to discuss a project with you.",
-    },
-    {
-      email: "olivia@example.com",
-      message: "Hi Olivia, how is your new startup going?",
-    },
-    {
-      email: "noah@example.com",
-      message: "Hello Noah, could you send me the details of the meeting?",
-    },
-    {
-      email: "emily@example.com",
+      email: "nicolas@example.com",
       message:
-        "Hi Emily, I just wanted to say that you've done an excellent job.",
-    },
-    {
-      email: "samuel@example.com",
-      message: "Hello Samuel, I have some questions regarding your service.",
-    },
-    {
-      email: "charlotte@example.com",
-      message:
-        "Hi Charlotte, do you have any availability for a meeting next week?",
-    },
-    {
-      email: "gabrielle@example.com",
-      message: "Hello Gabrielle, congratulations on your promotion!",
-    },
-    {
-      email: "leo@example.com",
-      message: "Hi Leo, I need your opinion on a project.",
-    },
-    {
-      email: "julia@example.com",
-      message: "Hello Julia, when are you available for a phone call?",
-    },
-    {
-      email: "lucas@example.com",
-      message: "Hi Lucas, I would like to invite you to our special event.",
-    },
-    {
-      email: "clara@example.com",
-      message: "Hello Clara, can you recommend a good restaurant in the area?",
-    },
-    {
-      email: "adam@example.com",
-      message: "Hi Adam, congratulations on your graduation!",
-    },
-    {
-      email: "huitre@nfeu.com",
-      message: "fee",
+        "Salut, je suis à la recherche d'un bureau fonctionnel pour mon espace de travail à domicile. Est-ce que vous avez des bureaux avec des tiroirs de rangement et des passe-câbles intégr",
     },
   ])
 }
