21b57e8db544de04db4b8a33f679473741d19df2
TamWilliam <72071669+TamWilliam@users.noreply.github.com>
2023-07-01 14:27:01 +0200

    translation for all public pages

---
+++ src/pages/forgot-password/reset/index.jsx
@@ -0,0 +1,92 @@
+import {
+  confirmPasswordValidator,
+  createValidator,
+  passwordValidator,
+} from "@/validators.js"
+import Head from "next/head"
+import Form from "@/web/components/Form.jsx"
+import FormField from "@/web/components/FormField.jsx"
+import SubmitButton from "@/web/components/SubmitButton.jsx"
+import useAppContext from "@/web/hooks/useAppContext.jsx"
+import { useCallback, useState } from "react"
+import { useRouter } from "next/router"
+import { useTranslation } from "react-i18next"
+
+const initialValues = {
+  password: "",
+  comfirmpassword: "",
+}
+const validationSchema = createValidator({
+  password: passwordValidator.required(),
+  comfirmpassword: confirmPasswordValidator.required(),
+})
+
+const ForgotPasswordResetPage = () => {
+  const { t } = useTranslation()
+  const router = useRouter()
+  const {
+    actions: { resetPassword },
+  } = useAppContext()
+  const [error, setError] = useState(null)
+  const { query } = router
+  const userId = query.id || null
+
+  const handleSubmit = useCallback(
+    async (values) => {
+      setError(null)
+
+      const [err] = await resetPassword(userId, values.password)
+
+      if (err) {
+        setError(err)
+
+        return
+      }
+
+      router.push("/sign-in")
+    },
+    [resetPassword, router, userId]
+  )
+
+  return (
+    <>
+      <Head>
+        <title>{t('forgotPasswordTitle')}</title>
+        <meta name="description" content="Contact us page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="mx-16 flex min-h-screen flex-col lg:mx-96">
+        <div className="my-20 flex justify-center text-xl font-bold lg:text-4xl">
+          {t('resetPassword')}
+        </div>
+        <div>
+          <Form
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+            error={error}
+          >
+            <FormField
+              name="password"
+              placeholder="Enter your new password"
+              label="Reset Password"
+              type="password"
+            />
+            <FormField
+              name="comfirmpassword"
+              placeholder="Enter your new password"
+              label="Confirm Reset Password"
+              type="password"
+            />
+
+            <SubmitButton className="mt-10"></SubmitButton>
+          </Form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+ForgotPasswordResetPage.isPublicPage = true
+export default ForgotPasswordResetPage
--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -5,21 +5,28 @@ import SubmitButton from "@/web/components/SubmitButton.jsx"
 import useAppContext from "@/web/hooks/useAppContext.jsx"
 import { useRouter } from "next/router.js"
 import { useCallback, useState } from "react"
+import Image from "next/image"
+import routes from "@/web/routes"
+import Link from "next/link"
+import { useTranslation } from "react-i18next"
 
 const initialValues = {
   email: "",
   password: "",
 }
+
 const validationSchema = createValidator({
   email: emailValidator.required(),
 })
 
 const SignInPage = () => {
+  const { t } = useTranslation()
   const router = useRouter()
   const {
     actions: { signIn },
   } = useAppContext()
   const [error, setError] = useState(null)
+
   const handleSubmit = useCallback(
     async (values) => {
       setError(null)
@@ -38,26 +45,64 @@ const SignInPage = () => {
   )
 
   return (
-    <Form
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={handleSubmit}
-      error={error}
-    >
-      <FormField
-        name="email"
-        placeholder="Enter your e-mail"
-        label="E-mail"
-        type="email"
-      />
-      <FormField
-        name="password"
-        placeholder="Enter your password"
-        label="Password"
-        type="password"
-      />
-      <SubmitButton>Sign In</SubmitButton>
-    </Form>
+    <div className="min-h-screen">
+      <div className="lg:grid lg:grid-cols-2">
+        <div className="hidden lg:col-span-1 lg:mb-8  lg:mt-40 lg:flex lg:justify-center">
+          <div className="flex max-h-80 items-center justify-center">
+            <Image
+              alt="canape"
+              width={700}
+              height={1000}
+              src="/images/canape.jpg"
+              className="ml-16 opacity-50"
+            />
+            <Image
+              alt="logo"
+              width={150}
+              height={150}
+              src="/images/Airneis.jpg"
+              className="absolute ml-16"
+            />
+          </div>
+        </div>
+
+        <div className="flex-col  lg:col-span-1">
+          <div className="mt-20 flex justify-center text-3xl font-bold">
+            {t('signIn')}
+          </div>
+          <div className="mx-auto mb-16 mt-8 max-w-md border-black">
+            <Form
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+              error={error}
+            >
+              <FormField
+                name="email"
+                placeholder={t('emailPlaceholder')}
+                label={t('emailLabel')}
+                type="email"
+              />
+              <FormField
+                name="password"
+                placeholder={t('passwordPlaceholder')}
+                label={t('passwordPlaceholder')}
+                type="password"
+              />
+              <SubmitButton className="mt-10">{t('signUp')}</SubmitButton>
+            </Form>
+            <div className="mt-4 text-center">
+              <Link
+                href={routes.forgotPassword()}
+                className="text-sm  underline"
+              >
+                {t('forgotPasswordLink')}
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
