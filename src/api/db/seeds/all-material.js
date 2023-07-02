b8e2b63d1f169811e100b733e260ca217d57ccf0
LouisLagrange1 <ljblagrange@gmail.com>
2023-07-02 23:32:58 +0200

    edit seed material

--- src/api/db/seeds/all-material.js
+++ src/api/db/seeds/all-material.js
@@ -3,11 +3,37 @@ exports.seed = async function (knex) {
   await knex("materials").insert([
     {
       name: "Bois",
-      slug: "canape",
+      slug: "bois",
     },
     {
+      name: "Chêne",
+      slug: "chene",
+    },
+    {
+      name: "Hêtre",
+      slug: "hetre",
+    },
+    {
+      name: "Pin",
+      slug: "pin",
+    },
+
+    {
       name: "Metal",
-      slug: "caeadzdnape",
+      slug: "metal",
+    },
+
+    {
+      name: "Acier",
+      slug: "acier",
+    },
+    {
+      name: "Fer forgé",
+      slug: "fer-forge",
+    },
+    {
+      name: "Aluminium",
+      slug: "aluminium",
     },
   ])
 }
